e95687f9418431f1aa3c5bd336b06f0db04fd04b
Marty <maruri2424@gmail.com>
2022-08-10 16:21:39 -0700

    added more fruit

--- src/Data/products.js
+++ src/Data/products.js
@@ -1,9 +1,37 @@
-import lemons from '../assets/lemons.jpg';
-import oranges from '../assets/oranges.webp';
+import lemon from '../assets/lemons.jpg';
+import orange from '../assets/oranges.webp';
+import lime from '../assets/lime.jpg';
+import grapefruit from '../assets/grapefruit.jpg';
+import yuzu from '../assets/yuzu.jpg';
+import bitterOrange from '../assets/bitterorange.webp';
+import bloodOrange from '../assets/bloodorange.webp';
+import calamansi from '../assets/calamansi.jpeg';
+import kumquat from '../assets/kumquat.jpg';
+import mandarin from '../assets/mandarin.webp';
+import pomelo from '../assets/pomelo.jpeg';
+import satsuma from '../assets/satsuma.jpeg';
+import ugli from '../assets/ugli.webp';
+
+
+
+
+
+
 
 const products = [
-  {name: 'lemons', img: lemons, price: '$ 5.00'},
-  {name: 'oranges', img: oranges, price: '$5.00'}
+  {name: 'lemons', img: lemon, price: '$ 5.00'},
+  {name: 'oranges', img: orange, price: '$5.00'},
+  {name: 'limes', img: lime, price: '$5.00'},
+  {name: 'grapefruit', img: grapefruit, price: '$5.00'},
+  {name: 'yuzu', img: yuzu, price: '$5.00'},
+  {name: 'bitter orange', img: bitterOrange, price: '$5.00'},
+  {name: 'blood orange', img: bloodOrange, price: '$5.00'},
+  {name: 'calamansi', img: calamansi, price: '$5.00'},
+  {name: 'kumquat', img: kumquat, price: '$5.00'},
+  {name: 'mandarin', img: mandarin, price: '$5.00'},
+  {name: 'pomelo', img: pomelo, price: '$5.00'},
+  {name: 'satsuma', img: satsuma, price: '$5.00'},
+  {name: 'ugli fruit', img: ugli, price: '$5.00'},
 ]
 
 export default products;
